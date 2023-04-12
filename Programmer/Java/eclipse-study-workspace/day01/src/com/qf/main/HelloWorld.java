bdbb743035f12b9c692ebdd58796f487d6777cfe
makuning <makun_ing@126.com>
2023-04-12 17:02:30 +0800

    add the first java program

---
+++ Programmer/Java/eclipse-study-workspace/day01/src/com/qf/main/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.qf.main;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.version"));
+		System.out.println("Hello World!!!");
+	}
+}
